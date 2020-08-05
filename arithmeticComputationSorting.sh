5647aac052656935bc1da6b9d512512e8975b12e
AlokSrivastava <aloksrivastava.0696@gmail.com>
2020-08-05 13:28:27 +0530

    Computing a+b*c

--- arithmeticComputationSorting.sh
+++ arithmeticComputationSorting.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -x
 
 echo "This problem computes different arithmetic expressions and sorts the results"
-read -p "Enter the first number:"a
-read -p "Enter the second number:"b
-read -p "Enter the third number:"c
+read -p "Enter the first number:" a
+read -p "Enter the second number:" b
+read -p "Enter the third number:" c
 
+echo $(($a+$b*$c))
