b63684f02b8210cefdf92076773f853b562a4f37
AlokSrivastava <aloksrivastava.0696@gmail.com>
2020-08-05 14:06:19 +0530

    Values of Dictionary is stored in array

--- arithmeticComputationSorting.sh
+++ arithmeticComputationSorting.sh
@@ -7,7 +7,13 @@ read -p "Enter the third number:" c
 
 declare -A Computations
 
-Computation[result1]=$(($a+$b*$c))
-Computation[result2]=$(($a*$b+$c))
-Computation[result3]=$(($c+$a/$b))
-Computation[result4]=$(($a%$b+$c))
+Computations[result1]=$(($a+$b*$c))
+Computations[result2]=$(($a*$b+$c))
+Computations[result3]=$(($c+$a/$b))
+Computations[result4]=$(($a%$b+$c))
+
+counter=0
+for value in `echo ${Computations[@]}`
+do
+	arrayResult[(( counter++ ))]=$value
+done
