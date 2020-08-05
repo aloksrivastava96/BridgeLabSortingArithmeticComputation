c3cc0edd97a7ca1a0387decb96cb5d37d0fb1b1c
AlokSrivastava <aloksrivastava.0696@gmail.com>
2020-08-05 14:19:14 +0530

    Results are sorted in Descending order

--- arithmeticComputationSorting.sh
+++ arithmeticComputationSorting.sh
@@ -12,8 +12,28 @@ Computations[result2]=$(($a*$b+$c))
 Computations[result3]=$(($c+$a/$b))
 Computations[result4]=$(($a%$b+$c))
 
+function arraySort() {
+        for (( sortcounter=0; sortcounter<LENGTH; sortcounter++ ))
+        do
+                for (( newcounter=$sortcounter+1; newcounter<LENGTH; newcounter++ ))
+                do
+                        if (( ${arrayResult[sortcounter]}<${arrayResult[newcounter]} ))
+                        then
+                                element=${arrayResult[sortcounter]}
+                                arrayResult[sortcounter]=${arrayResult[newcounter]}
+                                arrayResult[newcounter]=$element
+                        fi
+                done
+        done
+	echo ${arrayResult[@]}
+}
+
 counter=0
 for value in `echo ${Computations[@]}`
 do
 	arrayResult[(( counter++ ))]=$value
 done
+echo "Results in descending order are:"
+LENGTH=$((counter++))
+arraySort $arrayResult $LENGTH
+
